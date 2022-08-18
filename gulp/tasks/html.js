d497bfa6727108d6519cb02a4597f58ee8229713
Temur Radjabov <t.radjabov@andersenlab.com>
2022-08-18 16:33:17 +0300

    done

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,29 +5,33 @@ import versionNumber from "gulp-version-number";
 export const html = () => {
   return app.gulp
     .src(app.path.src.html)
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title:"HTML",
-        message: "Error <%= error.message %>"
-      })
-    ))
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "HTML",
+          message: "Error <%= error.message %>",
+        })
+      )
+    )
     .pipe(fileInclude())
     .pipe(app.plugins.replace(/@img\//g, "img/"))
-    .pipe(webpHtmlNosvg())
-    .pipe(versionNumber({
-      'value': '%DT%',
-      'append': {
-        'key': '_v',
-        'cover': 0,
-        'to': [
-          'css',
-          'js'
-        ]
-      },
-      'output': {
-        'file': 'gulp/vertion.json'
-      }
-    }))
+    .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        versionNumber({
+          value: "%DT%",
+          append: {
+            key: "_v",
+            cover: 0,
+            to: ["css", "js"],
+          },
+          output: {
+            file: "gulp/vertion.json",
+          },
+        })
+      )
+    )
     .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(app.plugins.browsersync.stream())
+    .pipe(app.plugins.browsersync.stream());
 };
